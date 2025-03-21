869a16ee2b0e847627d3500e6d32d9f94f8f0066
Felipe Rubio <felipe.rubio@alternova.com>
2025-03-21 02:44:16 -0500

    Project creation

---
+++ Habit Tracker/Habit_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Habit_TrackerApp.swift
+//  Habit Tracker
+//
+//  Created by Felipe on 21/03/25.
+//
+
+import SwiftUI
+
+@main
+struct Habit_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
